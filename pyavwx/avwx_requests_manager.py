42a8ecb4e50412d4cde144c6996e60471a60ce2c
Bastien1533 <bastien.pinaud@yahoo.com>
2023-11-03 19:15:47 +0100

    added methods

--- pyavwx/avwx_requests_manager.py
+++ pyavwx/avwx_requests_manager.py
@@ -5,8 +5,13 @@ from pyavwx.avwx_exceptions import AvwxBadStatus
 from pyavwx.avwx_authentication import AvwxApiAuth
 
 
-def makeRequest(url: str, auth: AvwxApiAuth, rjson: bool) -> tuple:
-    r = requests.get(url=url, auth=auth)
+def makeRequest(url: str, auth: AvwxApiAuth, data: str = None, rjson: bool = True, method: str = 'GET') -> tuple:
+    if method == 'GET':
+        r = requests.get(url=url, auth=auth, data=data)
+    elif method == 'POST':
+        r = requests.post(url=url, auth=auth, data=data)
+    else:
+        raise AttributeError("Method incorrect : not 'POST' or 'GET'")
     if r.status_code != requests.codes.ok:
         raise AvwxBadStatus(request=r)
     if rjson:
