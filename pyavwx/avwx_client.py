3834b1cdb530e73cbed073f99082bb987fd300f1
Bastien1533 <bastien.pinaud@yahoo.com>
2023-11-04 23:23:56 +0100

    Black formatting + parse_pirep

--- pyavwx/avwx_client.py
+++ pyavwx/avwx_client.py
@@ -14,84 +14,134 @@ class AvwxApiClient:
         self.auth = AvwxApiAuth(api_key)
 
     def get_metar(
-            self,
-            location: str,
-            options: str = None,
-            airport: bool = None,
-            reporting: bool = None,
-            remove: str = None,
-            filter: str = None,
-            onfail: str = None,
-            url_modifier: str = "metar/") -> Metar:
+        self,
+        location: str,
+        options: str = None,
+        airport: bool = None,
+        reporting: bool = None,
+        remove: str = None,
+        filter: str = None,
+        onfail: str = None,
+        url_modifier: str = "metar/",
+    ) -> Metar:
         args = locals()
-        url = url_builder(url_modifier=url_modifier, base_url=BASE_URL, main_payload=args['location'], args=args)
+        url = url_builder(
+            url_modifier=url_modifier,
+            base_url=BASE_URL,
+            main_payload=args["location"],
+            args=args,
+        )
 
         # We Make the request, evaluate the status code
-        # And then cast the json response to the Metar Object. 
-        r = makeRequest(url=url, auth=self.auth, rjson=True)[1]
-        return Metar(**r)
+        # And then cast the json response to the Metar Object.
+        r = makeRequest(url=url, auth=self.auth, rjson=True)
+        return Metar(**r[1])
 
     def parse_metar(
-            self,
-            metar: str = None,
-            options: str = None,
-            remove: str = None,
-            filter: str = None,
-            url_modifier: str = "parse/metar") -> Metar:
-        
+        self,
+        metar: str = None,
+        options: str = None,
+        remove: str = None,
+        filter: str = None,
+        url_modifier: str = "parse/metar",
+    ) -> Metar:
         args = locals()
-        url = url_builder(url_modifier=url_modifier, base_url=BASE_URL, main_payload=metar, include_main=False, args=args)
-        print(url)
+        url = url_builder(
+            url_modifier=url_modifier,
+            base_url=BASE_URL,
+            main_payload=metar,
+            include_main=False,
+            args=args,
+        )
         # We Make the request, evaluate the status code
-        # And then cast the json response to the Metar Object. 
-        r = makeRequest(url=url, auth=self.auth, rjson=True, data=metar, method='POST')[1]
-        return Metar(**r)
+        # And then cast the json response to the Metar Object.
+        r = makeRequest(url=url, auth=self.auth, rjson=True, data=metar, method="POST")
+        return Metar(**r[1])
 
     def get_taf(
-            self,
-            location: str,
-            options: str = None,
-            airport: bool = None,
-            reporting: bool = None,
-            remove: str = None,
-            filter: str = None,
-            onfail: str = None,
-            url_modifier: str = "taf/") -> Taf:
+        self,
+        location: str,
+        options: str = None,
+        airport: bool = None,
+        reporting: bool = None,
+        remove: str = None,
+        filter: str = None,
+        onfail: str = None,
+        url_modifier: str = "taf/",
+    ) -> Taf:
         args = locals()
-        url = url_builder(url_modifier=url_modifier, base_url=BASE_URL, main_payload=args['location'], args=args)
+        url = url_builder(
+            url_modifier=url_modifier,
+            base_url=BASE_URL,
+            main_payload=args["location"],
+            args=args,
+        )
 
         # We Make the request, evaluate the status code
-        # And then cast the json response to the Metar Object. 
-        r = makeRequest(url=url, auth=self.auth, rjson=True)[1]
-        return Taf(**r)
+        # And then cast the json response to the Metar Object.
+        r = makeRequest(url=url, auth=self.auth, rjson=True)
+        return Taf(**r[1])
 
     def parse_taf(
-            self,
-            taf: str = None,
-            options: str = None,
-            remove: str = None,
-            filter: str = None,
-            url_modifier: str = "parse/taf") -> Taf:
+        self,
+        taf: str = None,
+        options: str = None,
+        remove: str = None,
+        filter: str = None,
+        url_modifier: str = "parse/taf",
+    ) -> Taf:
         args = locals()
-        url = url_builder(url_modifier=url_modifier, base_url=BASE_URL, main_payload=taf, include_main=False, args=args)
-        print(url)
+        url = url_builder(
+            url_modifier=url_modifier,
+            base_url=BASE_URL,
+            main_payload=taf,
+            include_main=False,
+            args=args,
+        )
         # We Make the request, evaluate the status code
-        # And then cast the json response to the Metar Object. 
-        r = makeRequest(url=url, auth=self.auth, rjson=True, data=taf, method='POST')[1]
-        return Taf(**r)
+        # And then cast the json response to the Metar Object.
+        r = makeRequest(url=url, auth=self.auth, rjson=True, data=taf, method="POST")
+        return Taf(**r[1])
 
     def get_pirep(
-            self,
-            location: str,
-            options: str = None,
-            remove: str = None,
-            filter: str = None,
-            onfail: str = None,
-            url_modifier: str = "pirep/") -> Pirep:
+        self,
+        location: str,
+        options: str = None,
+        remove: str = None,
+        filter: str = None,
+        onfail: str = None,
+        url_modifier: str = "pirep/",
+    ) -> Pirep:
         args = locals()
-        url = url_builder(url_modifier=url_modifier, base_url=BASE_URL, main_payload=args['location'], args=args)
+        url = url_builder(
+            url_modifier=url_modifier,
+            base_url=BASE_URL,
+            main_payload=args["location"],
+            args=args,
+        )
 
         # We Make the request, evaluate the status code
-        # And then cast the json response to the Metar Object. 
-        r = makeRequest(url=url, auth=self.auth, rjson=True)[1]
-        return Pirep(**r)
+        # And then cast the json response to the Metar Object.
+        r = makeRequest(url=url, auth=self.auth, rjson=True)
+        return Pirep(**r[1])
+
+    def parse_pirep(
+        self,
+        pirep: str = None,
+        options: str = None,
+        remove: str = None,
+        filter: str = None,
+        url_modifier: str = "parse/pirep",
+    ) -> Pirep:
+        args = locals()
+        url = url_builder(
+            url_modifier=url_modifier,
+            base_url=BASE_URL,
+            main_payload=pirep,
+            include_main=False,
+            args=args,
+        )
+        # We Make the request, evaluate the status code
+        # And then cast the json response to the Metar Object.
+        r = makeRequest(url=url, auth=self.auth, rjson=True, data=pirep, method="POST")
+        return Pirep(**r[1])
