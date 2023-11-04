f4fa1d21d292232c46b765b6d9d19771699b0e3e
Bastien1533 <bastien.pinaud@yahoo.com>
2023-11-04 17:41:30 +0100

    added get_pirep()

--- pyavwx/avwx_client.py
+++ pyavwx/avwx_client.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 from pyavwx.avwx_authentication import AvwxApiAuth
 from pyavwx.models.utils import url_builder
-from pyavwx.models import Metar, Taf
+from pyavwx.models import Metar, Taf, Pirep
 from pyavwx.avwx_requests_manager import makeRequest
 from pyavwx.const import BASE_URL
 
@@ -79,3 +79,19 @@ class AvwxApiClient:
         # And then cast the json response to the Metar Object. 
         r = makeRequest(url=url, auth=self.auth, rjson=True, data=taf, method='POST')[1]
         return Taf(**r)
+
+    def get_pirep(
+            self,
+            location: str,
+            options: str = None,
+            remove: str = None,
+            filter: str = None,
+            onfail: str = None,
+            url_modifier: str = "pirep/") -> Pirep:
+        args = locals()
+        url = url_builder(url_modifier=url_modifier, base_url=BASE_URL, main_payload=args['location'], args=args)
+
+        # We Make the request, evaluate the status code
+        # And then cast the json response to the Metar Object. 
+        r = makeRequest(url=url, auth=self.auth, rjson=True)[1]
+        return Pirep(**r)
