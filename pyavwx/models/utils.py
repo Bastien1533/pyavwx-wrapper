cb4094f34dcacf245ccc48ab69a6bd7538088a19
Bastien1533 <bastien.pinaud@yahoo.com>
2023-11-06 23:04:04 +0100

    added list support for nested dataclass

--- pyavwx/models/utils.py
+++ pyavwx/models/utils.py
@@ -1,3 +1,4 @@
+import typing
 from dataclasses import dataclass, is_dataclass
 
 
@@ -9,6 +10,7 @@ def nested_dataclass(*args, **kwargs):
         def __init__(self, *args, **kwargs):
             for name, value in kwargs.items():
                 field_type = cls.__annotations__.get(name, None)
+
                 if is_dataclass(field_type) and isinstance(value, dict):
                     new_obj = field_type(
                         **{
@@ -16,6 +18,13 @@ def nested_dataclass(*args, **kwargs):
                             for key, value in value.items()
                             if key in field_type.__annotations__
                         }
+                    )
+                    kwargs[name] = new_obj
+                if type(value) == list:
+                    # ⇩⇩⇩⇩⇩⇩⇩ https://koor.fr/Python/API/python/types/GenericAlias/Index.wp ⇩⇩⇩⇩⇩⇩⇩
+                    list_type = field_type.__args__[0]
+                    new_obj = list(
+                        [list_type(**d) for d in value]
                     )
                     kwargs[name] = new_obj
             original_init(self, *args, **kwargs)
@@ -27,11 +36,11 @@ def nested_dataclass(*args, **kwargs):
 
 
 def url_builder(
-    url_modifier: str,
-    base_url: str,
-    args: dict,
-    main_payload: str,
-    include_main: bool = True,
+        url_modifier: str,
+        base_url: str,
+        args: dict,
+        main_payload: str,
+        include_main: bool = True,
 ) -> str:
     # Building URL:
     # To get something like that: metar?options=translate&format=json&remove=&filter=
