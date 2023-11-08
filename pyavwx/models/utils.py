ffa09c235e55127497a67df42787ea8573fd7b3c
Bastien1533 <bastien.pinaud@yahoo.com>
2023-11-08 23:58:35 +0100

    Added docs and emptylist checker

--- pyavwx/models/utils.py
+++ pyavwx/models/utils.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass, is_dataclass
 
 
+# This function wrapper enable dataclass to have other dataclass as types
+# it casts the dict coresponding to the nested dataclass in the types dataclass
+# if the Metar class has an info field typed to station, it will cast the dict coresponding to station.
+# The same goes for lists of dataclass.
 def nested_dataclass(*args, **kwargs):
     def wrapper(cls):
         cls = dataclass(cls, **kwargs)
@@ -9,6 +13,7 @@ def nested_dataclass(*args, **kwargs):
         def __init__(self, *args, **kwargs):
             for name, value in kwargs.items():
                 field_type = cls.__annotations__.get(name, None)
+                # if the value we want to cast in is a dict
                 if is_dataclass(field_type) and isinstance(value, dict):
                     new_obj = field_type(
                         **{
@@ -18,7 +23,9 @@ def nested_dataclass(*args, **kwargs):
                         }
                     )
                     kwargs[name] = new_obj
-                if type(value) == list:
+
+                # if the value we want to cast in is a list of dict
+                if type(value) == list and len(value) != 0:
                     # ⇩⇩⇩⇩⇩⇩⇩ https://koor.fr/Python/API/python/types/GenericAlias/Index.wp ⇩⇩⇩⇩⇩⇩⇩
                     list_type = field_type.__args__[0]
                     new_obj = list([list_type(**d) for d in value])
@@ -30,13 +37,13 @@ def nested_dataclass(*args, **kwargs):
 
     return wrapper(args[0]) if args else wrapper
 
-
+# A reusable url builder tailored for this wrapper
 def url_builder(
-    url_modifier: str,
-    base_url: str,
-    args: dict,
-    main_payload: str,
-    include_main: bool = True,
+        url_modifier: str,
+        base_url: str,
+        args: dict,
+        main_payload: str,
+        include_main: bool = True,
 ) -> str:
     # Building URL:
     # To get something like that: metar?options=translate&format=json&remove=&filter=
@@ -50,5 +57,10 @@ def url_builder(
     for arg in args:
         value = args.get(arg)
         if value and value != main_payload:
+            if type(value) == bool:
+                if value:
+                    value = "true"
+                else:
+                    value = "false"
             url = url + f'{arg}={str(value).replace(" ", "")}&'
     return url
