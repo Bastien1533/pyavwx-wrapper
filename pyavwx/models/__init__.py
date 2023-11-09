3de73e9e51a8d083aebfccfff7f87c424decb992
Bastien1533 <bastien.pinaud@yahoo.com>
2023-11-09 11:42:53 +0100

    Added reports around a route objects

--- pyavwx/models/__init__.py
+++ pyavwx/models/__init__.py
@@ -3,7 +3,7 @@ from .metar import Metar
 from .nbm_gfs import Nbm, Gfs
 from .notams import Notam
 from .pirep import Pirep
-from .station import NearStation, StationRoute
+from .station import NearStation, StationRoute, ReportsRoute
 from .structs import Station
 from .summary import Summary
 from .taf import Taf
--- pyavwx/models/station.py
+++ pyavwx/models/station.py
@@ -1,7 +1,11 @@
 import json
 from dataclasses import dataclass
 
+from pyavwx.models.airSigmet import AirSigmet
+from pyavwx.models.metar import Metar
+from pyavwx.models.notams import Notam
 from pyavwx.models.structs import Meta, Station
+from pyavwx.models.taf import Taf
 from pyavwx.models.utils import nested_dataclass
 
 
@@ -32,6 +36,19 @@ class StationRoute:
     meta: Meta = None
     route: list[Route] = None
     results: list[Station] = None
+
+    def to_dict(self):
+        return self.__dict__
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
+
+
+@nested_dataclass
+class ReportsRoute:
+    meta: Meta = None
+    route: list[Route] = None
+    results: Metar | Taf | AirSigmet | Notam = None
 
     def to_dict(self):
         return self.__dict__
