8f6d02ecf46d3dc758f0fc1c0324247fe6876143
Bastien1533 <bastien.pinaud@yahoo.com>
2023-11-09 11:46:24 +0100

    Added "Multiple Reports", "Nearest Reports", "Report Text Search", "Reports around a Route"

--- pyavwx/avwx_client.py
+++ pyavwx/avwx_client.py
@@ -12,7 +12,8 @@ from pyavwx.models import (
     AirSigmet,
     Notam,
     Nbm,
-    Gfs
+    Gfs,
+    ReportsRoute,
 )
 from pyavwx.models.utils import url_builder
 
@@ -22,11 +23,11 @@ class AvwxApiClient:
         self.auth = AvwxApiAuth(api_key)
 
     def get_station(
-        self,
-        ident: str,
-        remove: str = None,
-        filter: str = None,
-        url_modifier: str = "station/",
+            self,
+            ident: str,
+            remove: str = None,
+            filter: str = None,
+            url_modifier: str = "station/",
     ) -> Station:
         args = locals()
         url = url_builder(
@@ -42,12 +43,12 @@ class AvwxApiClient:
         return Station(**r[1])
 
     def get_near_stations(
-        self,
-        coords: str,
-        n: int = 10,
-        remove: str = None,
-        filter: str = None,
-        url_modifier: str = "station/near/",
+            self,
+            coords: str,
+            n: int = 10,
+            remove: str = None,
+            filter: str = None,
+            url_modifier: str = "station/near/",
     ) -> list[NearStation]:
         args = locals()
         url = url_builder(
@@ -67,12 +68,12 @@ class AvwxApiClient:
         return station_list
 
     def get_stations_text(
-        self,
-        text: str,
-        n: str = "10",
-        remove: str = None,
-        filter: str = None,
-        url_modifier: str = "search/station",
+            self,
+            text: str,
+            n: int,
+            remove: str = None,
+            filter: str = None,
+            url_modifier: str = "search/station",
     ) -> list[Station]:
         args = locals()
         url = url_builder(
@@ -91,12 +92,12 @@ class AvwxApiClient:
         return station_list
 
     def get_stations_route(
-        self,
-        route: str,
-        distance: int,
-        remove: str = None,
-        filter: str = None,
-        url_modifier: str = "path/station",
+            self,
+            route: str,
+            distance: int,
+            remove: str = None,
+            filter: str = None,
+            url_modifier: str = "path/station",
     ) -> StationRoute:
         args = locals()
         url = url_builder(
@@ -112,19 +113,19 @@ class AvwxApiClient:
         return StationRoute(**r[1])
 
     def get_summary(
-        self,
-        location: str,
-        options: str = None,
-        remove: str = None,
-        filter: str = None,
-        onfail: str = None,
-        url_modifier: str = "summary/",
+            self,
+            location: str,
+            options: str = None,
+            remove: str = None,
+            filter: str = None,
+            onfail: str = None,
+            url_modifier: str = "summary/",
     ) -> Summary:
         args = locals()
         url = url_builder(
             url_modifier=url_modifier,
             base_url=BASE_URL,
-            main_payload=args["location"],
+            main_payload=location,
             args=args,
         )
         # We Make the request, evaluate the status code
@@ -133,21 +134,21 @@ class AvwxApiClient:
         return Summary(**r[1])
 
     def get_metar(
-        self,
-        location: str,
-        options: str = None,
-        airport: bool = None,
-        reporting: bool = None,
-        remove: str = None,
-        filter: str = None,
-        onfail: str = None,
-        url_modifier: str = "metar/",
+            self,
+            location: str,
+            options: str = None,
+            airport: bool = None,
+            reporting: bool = None,
+            remove: str = None,
+            filter: str = None,
+            onfail: str = None,
+            url_modifier: str = "metar/",
     ) -> Metar:
         args = locals()
         url = url_builder(
             url_modifier=url_modifier,
             base_url=BASE_URL,
-            main_payload=args["location"],
+            main_payload=location,
             args=args,
         )
 
@@ -157,12 +158,12 @@ class AvwxApiClient:
         return Metar(**r[1])
 
     def parse_metar(
-        self,
-        metar: str = None,
-        options: str = None,
-        remove: str = None,
-        filter: str = None,
-        url_modifier: str = "parse/metar",
+            self,
+            metar: str = None,
+            options: str = None,
+            remove: str = None,
+            filter: str = None,
+            url_modifier: str = "parse/metar",
     ) -> Metar:
         args = locals()
         url = url_builder(
@@ -177,38 +178,146 @@ class AvwxApiClient:
         r = makeRequest(url=url, auth=self.auth, rjson=True, data=metar, method="POST")
         return Metar(**r[1])
 
+    def get_multiple_reports(
+            self,
+            report_type: str,
+            locations: str,
+            options: str = None,
+            remove: str = None,
+            filter: str = None,
+            onfail: str = None,
+            url_modifier: str = "multi/",
+    ) -> Metar | Taf | Summary:
+        args = locals()
+        url = url_builder(
+            url_modifier=f"{url_modifier}{report_type}/",
+            base_url=BASE_URL,
+            main_payload=locations,
+            args=args,
+        )
+        # We Make the request, evaluate the status code
+        # And then cast the json response to the Metar Object.
+        r = makeRequest(url=url, auth=self.auth, rjson=True)
+        match report_type:
+            case "metar":
+                return Metar(**r[1])
+            case "taf":
+                return Taf(**r[1])
+            case "summary":
+                return Summary(**r[1])
+
+    def get_nearest_reports(
+            self,
+            report_type: str,
+            coords: str,
+            n: int,
+            options: str = None,
+            airport: bool = None,
+            remove: str = None,
+            filter: str = None,
+            onfail: str = None,
+            url_modifier: str = "near/",
+    ) -> Metar | Taf:
+        args = locals()
+        url = url_builder(
+            url_modifier=f"{report_type}/{url_modifier}",
+            base_url=BASE_URL,
+            main_payload=coords,
+            args=args,
+        )
+        # We Make the request, evaluate the status code
+        # And then cast the json response to the Metar Object.
+        r = makeRequest(url=url, auth=self.auth, rjson=True)
+        match report_type:
+            case "metar":
+                return Metar(**r[1])
+            case "taf":
+                return Taf(**r[1])
+
+    def get_reports_text(
+            self,
+            report_type: str,
+            text: str,
+            n: int,
+            options: str = None,
+            remove: str = None,
+            filter: str = None,
+            url_modifier: str = "search/",
+    ) -> list[Metar] | list[Taf]:
+        args = locals()
+        url = url_builder(
+            url_modifier=f"{url_modifier}",
+            base_url=BASE_URL,
+            main_payload=report_type,
+            args=args,
+        )
+        # We Make the request, evaluate the status code
+        # And then cast the json response to the Metar Object.
+        r = makeRequest(url=url, auth=self.auth, rjson=True)
+
+        report_list = []
+        match report_type:
+            case "metar":
+                for metar in r[1]:
+                    report_list.append(Metar(**metar))
+            case "taf":
+                for taf in r[1]:
+                    report_list.append(Taf(**taf))
+        return report_list
+
+    def get_reports_route(
+            self,
+            report_type: str,
+            route: str,
+            distance: int,
+            options: str = None,
+            remove: str = None,
+            filter: str = None,
+            onfail: str = None,
+            url_modifier: str = "path/",
+    ) -> ReportsRoute:
+        args = locals()
+        url = url_builder(
+            url_modifier=f"{url_modifier}",
+            base_url=BASE_URL,
+            main_payload=report_type,
+            args=args,
+        )
+        # We Make the request, evaluate the status code
+        # And then cast the json response to the Metar Object.
+        r = makeRequest(url=url, auth=self.auth, rjson=True)
+        return ReportsRoute(**r[1])
+
     def get_taf(
-        self,
-        location: str,
-        options: str = None,
-        airport: bool = None,
-        reporting: bool = None,
-        remove: str = None,
-        filter: str = None,
-        onfail: str = None,
-        url_modifier: str = "taf/",
+            self,
+            location: str,
+            options: str = None,
+            airport: bool = None,
+            reporting: bool = None,
+            remove: str = None,
+            filter: str = None,
+            onfail: str = None,
+            url_modifier: str = "taf/",
     ) -> Taf:
         args = locals()
         url = url_builder(
             url_modifier=url_modifier,
             base_url=BASE_URL,
-            main_payload=args["location"],
+            main_payload=location,
             args=args,
         )
-        print(url)
-
         # We Make the request, evaluate the status code
         # And then cast the json response to the Metar Object.
         r = makeRequest(url=url, auth=self.auth, rjson=True)
         return Taf(**r[1])
 
     def parse_taf(
-        self,
-        taf: str = None,
-        options: str = None,
-        remove: str = None,
-        filter: str = None,
-        url_modifier: str = "parse/taf",
+            self,
+            taf: str = None,
+            options: str = None,
+            remove: str = None,
+            filter: str = None,
+            url_modifier: str = "parse/taf",
     ) -> Taf:
         args = locals()
         url = url_builder(
@@ -224,19 +333,19 @@ class AvwxApiClient:
         return Taf(**r[1])
 
     def get_pirep(
-        self,
-        location: str,
-        options: str = None,
-        remove: str = None,
-        filter: str = None,
-        onfail: str = None,
-        url_modifier: str = "pirep/",
+            self,
+            location: str,
+            options: str = None,
+            remove: str = None,
+            filter: str = None,
+            onfail: str = None,
+            url_modifier: str = "pirep/",
     ) -> Pirep:
         args = locals()
         url = url_builder(
             url_modifier=url_modifier,
             base_url=BASE_URL,
-            main_payload=args["location"],
+            main_payload=location,
             args=args,
         )
 
@@ -246,12 +355,12 @@ class AvwxApiClient:
         return Pirep(**r[1])
 
     def parse_pirep(
-        self,
-        pirep: str = None,
-        options: str = None,
-        remove: str = None,
-        filter: str = None,
-        url_modifier: str = "parse/pirep",
+            self,
+            pirep: str = None,
+            options: str = None,
+            remove: str = None,
+            filter: str = None,
+            url_modifier: str = "parse/pirep",
     ) -> Pirep:
         args = locals()
         url = url_builder(
@@ -267,11 +376,11 @@ class AvwxApiClient:
         return Pirep(**r[1])
 
     def get_airsigmet(
-        self,
-        remove: str = None,
-        filter: str = None,
-        onfail: str = None,
-        url_modifier: str = "airsigmet",
+            self,
+            remove: str = None,
+            filter: str = None,
+            onfail: str = None,
+            url_modifier: str = "airsigmet",
     ) -> AirSigmet:
         args = locals()
         url = url_builder(
@@ -287,12 +396,12 @@ class AvwxApiClient:
         return AirSigmet(**r[1])
 
     def parse_airsigmet(
-        self,
-        airsigmet: str = None,
-        options: str = None,
-        remove: str = None,
-        filter: str = None,
-        url_modifier: str = "parse/airsigmet",
+            self,
+            airsigmet: str = None,
+            options: str = None,
+            remove: str = None,
+            filter: str = None,
+            url_modifier: str = "parse/airsigmet",
     ) -> AirSigmet:
         args = locals()
         url = url_builder(
@@ -310,13 +419,13 @@ class AvwxApiClient:
         return AirSigmet(**r[1])
 
     def get_notam(
-        self,
-        location: str,
-        distance: int = None,
-        remove: str = None,
-        filter: str = None,
-        onfail: str = "cache",
-        url_modifier: str = "notam/",
+            self,
+            location: str,
+            distance: int = None,
+            remove: str = None,
+            filter: str = None,
+            onfail: str = "cache",
+            url_modifier: str = "notam/",
     ) -> Notam:
         args = locals()
         url = url_builder(
@@ -332,11 +441,11 @@ class AvwxApiClient:
         return Notam(**r[1])
 
     def parse_notam(
-        self,
-        notam: str,
-        remove: str = None,
-        filter: str = None,
-        url_modifier: str = "parse/notam",
+            self,
+            notam: str,
+            remove: str = None,
+            filter: str = None,
+            url_modifier: str = "parse/notam",
     ) -> Notam:
         args = locals()
         url = url_builder(
@@ -352,16 +461,16 @@ class AvwxApiClient:
         return Notam(**r[1])
 
     def get_nbm(
-        self,
-        report: str,
-        location: str,
-        options: str = None,
-        airport: bool = None,
-        reporting: bool = None,
-        remove: str = None,
-        filter: str = None,
-        onfail: str = "cache",
-        url_modifier: str = "nbm/",
+            self,
+            report: str,
+            location: str,
+            options: str = None,
+            airport: bool = None,
+            reporting: bool = None,
+            remove: str = None,
+            filter: str = None,
+            onfail: str = "cache",
+            url_modifier: str = "nbm/",
     ) -> Nbm:
         args = locals()
         url = url_builder(
@@ -376,13 +485,13 @@ class AvwxApiClient:
         return Nbm(**r[1])
 
     def parse_nbm(
-        self,
-        nbm: str,
-        report: str,
-        options: str,
-        remove: str = None,
-        filter: str = None,
-        url_modifier: str = "parse/nbm",
+            self,
+            nbm: str,
+            report: str,
+            options: str = None,
+            remove: str = None,
+            filter: str = None,
+            url_modifier: str = "parse/nbm",
     ) -> Nbm:
         args = locals()
         url = url_builder(
@@ -425,7 +534,7 @@ class AvwxApiClient:
             self,
             gfs: str,
             report: str,
-            options: str,
+            options: str = None,
             remove: str = None,
             filter: str = None,
             url_modifier: str = "parse/gfs",
